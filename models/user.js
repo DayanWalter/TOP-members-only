c82b3b6593a5b66f4485fe645c3b641cd7c0e3c6
SynthCyraX <SynthCyraX@gmail.com>
2023-11-05 13:12:48 +0100

    Added UserSchema

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, minLength: 1, maxLength: 10 },
+  last_name: { type: String, required: true, minLength: 1, maxLength: 10 },
+  username: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 12,
+    unique: true,
+  },
+  password: { type: String, required: true },
+  status: { type: String },
+});
+
+// Virtual for URL
+UserSchema.virtual('url').get(function () {
+  return `/placeholder/${this._id}`;
+});
+
+// Virtual for fullname
+UserSchema.virtual('fullname').get(function () {
+  // To avoid errors in cases where an user does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
+  let fullname = '';
+  if (this.first_name && this.last_name) {
+    fullname = `${this.last_name}, ${this.first_name}`;
+  }
+  return fullname;
+});
+
+module.exports = mongoose.model('User', UserSchema);
