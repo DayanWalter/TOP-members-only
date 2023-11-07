5b501c0525d7432fe88a9815079566d3e182531e
SynthCyraX <SynthCyraX@gmail.com>
2023-11-07 04:16:26 +0100

    Changed form to POST

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,3 +17,20 @@ exports.sign_up_get = asyncHandler(async (req, res, next) => {
     title: 'Please Sign Up',
   });
 });
+
+// Sign-Up POST
+exports.sign_up_post = asyncHandler(async (req, res, next) => {
+  // Extract the validation errors from a request.
+
+  // Create a Book object with escaped and trimmed data.
+  const user = new User({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    username: req.body.username,
+    password: req.body.password,
+    status: req.body.status,
+  });
+
+  await user.save();
+  res.redirect('/');
+});
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const user_controller = require('../controllers/userController');
 const message_controller = require('../controllers/messageController');
 /* GET home page. */
 router.get('/', user_controller.index);
-
+// GET Signup
 router.get('/signup', user_controller.sign_up_get);
+
+//POST Signup
+router.post('/signup', user_controller.sign_up_post);
 
 module.exports = router;
