601d20baf0d1d4efa50f336ba135a08b8108e64c
SynthCyraX <SynthCyraX@gmail.com>
2023-11-07 04:29:20 +0100

    Installed bcrypt, express-session, passport and passport-local

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
 const Message = require('../models/message');
 
+const bcrypt = require('bcryptjs');
+
 const { body, validationResult } = require('express-validator');
 
 const asyncHandler = require('express-async-handler');
@@ -22,15 +24,20 @@ exports.sign_up_get = asyncHandler(async (req, res, next) => {
 exports.sign_up_post = asyncHandler(async (req, res, next) => {
   // Extract the validation errors from a request.
 
-  // Create a Book object with escaped and trimmed data.
-  const user = new User({
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    username: req.body.username,
-    password: req.body.password,
-    status: req.body.status,
-  });
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    try {
+      const user = new User({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        username: req.body.username,
+        password: hashedPassword,
+        status: req.body.status,
+      });
 
-  await user.save();
-  res.redirect('/');
+      await user.save();
+      res.redirect('/');
+    } catch (err) {
+      return next(err);
+    }
+  });
 });
